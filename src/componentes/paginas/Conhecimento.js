49484411131aa3105bb4d3fdc96ec848f671ca40
Amorim-EA <erick.amorim@estudante.ifms.edu.br>
2023-02-09 19:27:24 -0400

    Commit Parcial Final II

--- src/componentes/paginas/Conhecimento.js
+++ src/componentes/paginas/Conhecimento.js
@@ -5,15 +5,15 @@ const Conhecimento = () => {
      <div class="conhecimentos">
       <h1>Conhecimentos</h1>
       <h5>Desenvolvimento web - paginas responsivas</h5>
-      <h5>Desenvolvimento mobile - react-lite e SQLLite - estou começando</h5>
-      <h5>Automação em python com a biblioteca pyautogui, piperclip</h5>
+      <h5>Desenvolvimento mobile - React e SQLLite - estou começando</h5>
+      <h5>Automação em python com a biblioteca pyautogui, pyperclip</h5>
       <h5>Automação Web em python com a biblioteca Selenium</h5>
       <h5>Framework dominados: Bootstrap</h5>
       <h5>Bibliotecas: React, Pandas, ExpressJs</h5>
       <h5>Linguagens: JavaScript, nodejs, python, css, html, SQL</h5>
       <h5>Banco de Dados: Relacional(SQL), Não Relacional(mongodb, json, redis(desisti))</h5>
       <h5>Seguranças da Informação: Atualmente estudando WebHacking por conta própria</h5>
-      <h5>Domíno muito bem o SO Linux por linha de comando, automação em bash</h5>
+      <h5>Domíno muito bem o SO Linux por linha de comando, e automação em bash</h5>
       <h5>Domíno ferramentas como: MicroftWord, Excel, PowerPoint, Drive(documents)</h5>
     </div>
     )
--- src/componentes/paginas/Contato.js
+++ src/componentes/paginas/Contato.js
@@ -5,10 +5,10 @@ const Contato = () => {
     <div className="contato">
      <h1>Contato</h1>
      <h5>Email pessoal: erickdasilvaamorim@gmail.com</h5>
-     <h5>Email institucional: erick.amorim@estudante.edu.br</h5>
+     <h5>Email institucional: erick.amorim@estudante.ifms.edu.br</h5>
      <h5>Telefone: +55 67 9 93329022</h5>
      <h5>Facebook: Erick Amorim</h5>
-     <h5>Intagram: @3r1ck_4m0r1m</h5>
+     <h5>Instagram: @3r1ck_4m0r1m</h5>
      <h5>LinkedIn: EricK AmoriM</h5>
     </div>
     )
